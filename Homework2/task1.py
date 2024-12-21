3fca6f4a52faf016994b553593dafdc351b3d2d1
Kadyrbekovzhusup <kadyrbekovzhusup@gmail.com>
2024-12-21 05:00:03 +0000

    homework2

---
+++ Homework2/task1.py
@@ -0,0 +1,8 @@
+def sum(a, b, c, d ):
+    
+    return year*a, year*b, year*c, year*d
+
+year=int(input("Provide year: "))
+
+hi = sum(365, 52, 12, 8760 )
+print(hi)
---
+++ Homework2/task2.py
@@ -0,0 +1,9 @@
+def sum(a, b):
+    return a+b, a-b, a*b, a/b
+num1=int(input("Enter the number: "))
+num2=int(input("Enter the number: "))
+
+hi = sum(num1, num2)
+print(hi)
+print("There we added, subtracted, ultiplied, divided your numbers")
+    
